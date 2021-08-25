cb24b90b0df6321d2ecadb52e89191174389b9b2
santiago <santiago.kenshinvaldez@gmail.com>
2021-08-25 18:25:06 -0400

    Add handling for heroku production db

--- src/db/init.ts
+++ src/db/init.ts
@@ -1,10 +1,22 @@
 import { Sequelize } from "sequelize-typescript";
 import configFile from "../config.json";
+import parseDatabaseUrl from "../utils/parse-database";
 const env = process.env.NODE_ENV || "development";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const config = (<any>configFile)[env];
 
 export const initDB = (): Sequelize => {
+	if (env === "production") {
+		const options = parseDatabaseUrl(config.databaseUrl);
+		return new Sequelize({
+			dialect: "postgres",
+			protocol: "postgres",
+			logging: false,
+			...options,
+			modelPaths: [__dirname + "/models"]
+		})
+	}
+
 	return new Sequelize({
 		...config,
 		modelPaths: [__dirname + "/models"],
